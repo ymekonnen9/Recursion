6abb686da6b4e2b23f7a71d1495193dbd1044943
ymekonnen9 <yaredmekonnen999@gmail.com>
2024-05-15 18:12:30 -0500

    BinarySearch with recursion

---
+++ BinarySearch.java
@@ -0,0 +1,15 @@
+public class BinarySearch{
+
+    public static boolean binarySearch(int[] data, int target, int low, int high){
+        if(low > high){return false;}
+        int mid = (high + low)/2;
+        if(target == data[mid]){
+            return true;
+        }else if(target < data[mid]){
+            return binarySearch(data, target, low,mid-1);
+        }else{
+            return binarySearch(data, target, mid + 1, high);
+        }
+
+    }
+}
