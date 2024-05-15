69824fd2b4dbd2ae4a65ad704d20ebcd21400999
ymekonnen9 <yaredmekonnen999@gmail.com>
2024-05-15 18:41:34 -0500

    Linear recursion sum

---
+++ linearSum.java
@@ -0,0 +1,11 @@
+public class linearSum {
+
+    public static int linearSum(int[] data, int n){
+        if(n==0){
+            return 0;
+        }else{
+            return linearSum(data, n-1) + data[n-1];
+        }
+    }
+    
+}
